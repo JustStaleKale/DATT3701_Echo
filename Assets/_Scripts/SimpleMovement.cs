89a98d15d9f6e3f73644a10036668f3df67cc431
Soracole27 <Soracole27@gmail.com>
2026-02-02 21:25:06 -0500

    added basic ui and stuff

---
+++ Assets/_Scripts/SimpleMovement.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class SimpleMovement : MonoBehaviour
+{
+    PlayerInput playerInput;
+    InputAction inputAction;
+    new Rigidbody rigidbody;
+
+    [SerializeField] private float moveSpeed = 5f;
+
+    void Start()
+    {
+        playerInput = GetComponent<PlayerInput>();
+        inputAction = playerInput.actions.FindAction("Move");
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        MovePlayer();
+    }
+
+    void MovePlayer()
+    {
+        Vector2 direction = inputAction.ReadValue<Vector2>();
+        rigidbody.linearVelocity = new Vector3(direction.x, 0, direction.y) * moveSpeed;
+    }
+}
