8e36a700ad8acdd110f4718a1169b692dce0ef74
naug-nevan-216527657 <nevan@my.yorku.ca>
2026-03-16 22:02:49 -0400

    Fixed scene transitions

--- Assets/_Scripts/ManagerScripts/LevelManager.cs
+++ Assets/_Scripts/ManagerScripts/LevelManager.cs
@@ -1,4 +1,5 @@
 using System.Threading.Tasks;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -7,16 +8,18 @@ public class LevelManager : MonoBehaviour
 {
     public static LevelManager Instance;
     //public GameEvent setScene;
-    public Canvas loadCanvas;
-    public GameObject fillBar;
+    public GameObject loadCanvas;
+    public Slider slider;
+    private CanvasGroup loadCanvasGroup;
+    private float fadeDuration = 1f;
     //public Image panelImage;
     private string sceneName;
     //private bool isLevel2Complete = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        //setScene.Raise(this, SceneManager.GetActiveScene().name);
-    }
+    // void Start()
+    // {
+    //     //setScene.Raise(this, SceneManager.GetActiveScene().name);
+    // }
     
     void Awake()
     {
@@ -28,55 +31,83 @@ public class LevelManager : MonoBehaviour
         {
             Destroy(gameObject);
         }
+
+        loadCanvasGroup = loadCanvas?.GetComponent<CanvasGroup>();
+    }
+    private IEnumerator FadeIn(float start, float end)
+    {
+        float elapsedTime = 0f;
+        while (elapsedTime < fadeDuration)
+        {
+            loadCanvasGroup.alpha = Mathf.Lerp(start, end, elapsedTime / fadeDuration);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+        loadCanvasGroup.alpha = end;
     }
 
-    private async void LoadScene(string sceneName)
+    private async Task LoadScene(string sceneName)
     {
         var scene = SceneManager.LoadSceneAsync(sceneName);
         scene.allowSceneActivation = false;
         
-        loadCanvas.enabled = true;
+        loadCanvas.SetActive(true);
+        StartCoroutine(FadeIn(0f, 1f));
+        await Task.Delay(1000);
+        // panel.GetComponent<Image>().CrossFadeAlpha(0.99f, 0.5f, true);
+        //await Task.Delay(1000);
 
         do
         {
             await Task.Delay(100);
-            fillBar.GetComponent<Slider>().value = scene.progress / 0.9f;
+            slider.value = scene.progress / 0.9f;
         }while (scene.progress < 0.9f);
 
+        
         scene.allowSceneActivation = true;
+        StartCoroutine(FadeIn(1f, 0f));
         await Task.Delay(1000);
-        loadCanvas.enabled = false;
+        //await Task.Delay(1000);
+        
+        // panel.GetComponent<Image>().CrossFadeAlpha(0.01f, 0.5f, true);
+        loadCanvas.SetActive(false);
         //setScene.Raise(this, SceneManager.GetActiveScene().name);
     } 
 
     public async void LoadScene(Component sender, object data)
     {
-        LoadScene((string) data);
+        await LoadScene((string) data);
     } 
 
-    public void NextScene()
+    public async void NextScene()
     {
         Scene currentScene = SceneManager.GetActiveScene();
         sceneName = currentScene.name;
         
         if(sceneName == "Main Menu")
         {
-            LoadScene("TutorialLevel");
+            await LoadScene("TutorialLevel");
 
         } else if (sceneName == "TutorialLevel")
         {
-            LoadScene("Level 1");
+            await LoadScene("Second Scene");
 
-        } 
+        } else if (sceneName == "Second Scene")
+        {
+            await LoadScene("Third Scene");
+        } else if (sceneName == "Third Scene")
+        {
+            await LoadScene("Level 1");
+        }
 
-        if (sceneName == "Main Menu")
-        {
-            LoadScene("TutorialLevel");
-        }
-         else
-        {
-            LoadScene("Level 1");
-        }
+        // if (sceneName == "Main Menu")
+        // {
+        //     LoadScene("TutorialLevel");
+        // }
+        //  else
+        // {
+        //     LoadScene("Level 1");
+        // }
     }
 
     public void QuitGame()
@@ -95,9 +126,9 @@ public class LevelManager : MonoBehaviour
          
     // }
 
-    public void RestartScene(Component sender, object data)
+    public async void RestartScene(Component sender, object data)
     {
         Scene currentScene = SceneManager.GetActiveScene();
-        LoadScene(currentScene.name);
+        await LoadScene(currentScene.name);
     }
 }
