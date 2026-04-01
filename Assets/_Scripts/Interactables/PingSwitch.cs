d2e7f9d097200644fe17631c68387d41e3b3030a
naug-nevan-216527657 <nevan@my.yorku.ca>
2026-03-31 21:22:49 -0400

    Fixed doors and door lights

--- Assets/_Scripts/Interactables/PingSwitch.cs
+++ Assets/_Scripts/Interactables/PingSwitch.cs
@@ -6,12 +6,22 @@ public class PingSwitch : MonoBehaviour
     private Animator doorAnimator;
 
     public Light batteryLight;
+
+    public GameObject doorLightGO;
+    public Light doorLight;
+    private Renderer lightRenderer;
     private bool hasTriggered;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         doorAnimator = door.GetComponent<Animator>();
         hasTriggered = false;
+        batteryLight.color = Color.red;
+        doorLight.color = Color.red;
+        lightRenderer = doorLightGO.GetComponent<Renderer>();
+        lightRenderer.material.SetColor("_EmissionColor", Color.red);
+        door.gameObject.layer = LayerMask.NameToLayer("Invisible");
+        doorLightGO.layer = LayerMask.NameToLayer("Invisible");
         //batteryLight = GetComponent<Light>();
     }
 
@@ -20,7 +30,11 @@ public class PingSwitch : MonoBehaviour
         if (!hasTriggered)
         {
             batteryLight.color = Color.green;
+            doorLight.color = Color.green;
+            lightRenderer.material.SetColor("_EmissionColor", Color.green);
             doorAnimator.SetBool("character_nearby", true);
+            door.gameObject.layer = LayerMask.NameToLayer("Default");
+            doorLightGO.layer = LayerMask.NameToLayer("Default");
             hasTriggered = true;
         }
         
